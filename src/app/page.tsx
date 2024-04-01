7a83cf041d3021185e74089a79aeb5bdb93cf3b4
Mark Landeryou <samurai96@hotmail.com>
2024-04-01 02:10:14 -0400

    refactor: created a footer component and added it to the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="container mx-3 border-2 px-3">
-      <h1 className="text-center font-serif text-lg font-bold">
+    <main className="container mx-auto border-2 px-3">
+      <h1 className="text-center font-serif text-3xl font-bold">
         Welcome to my NextJs Starter Template
       </h1>
     </main>
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+//import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-2 border-2">
+      <p className="text-center">
+        &copy; {new Date().getFullYear()} All rights reserved.
+      </p>
+    </footer>
+  );
+}

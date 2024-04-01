b43d737101ca1a08eceab7290ba73342f995795f
Mark Landeryou <samurai96@hotmail.com>
2024-04-01 03:24:05 -0400

    feat: got the dark and  light themes working

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ThemeToggle } from "@/components/Header/theme-toggle";
+// import Link from "next/link";
 
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="text-center">
+      Header
+      <ThemeToggle />
+    </header>
+  );
 }
 
 export default Header;
